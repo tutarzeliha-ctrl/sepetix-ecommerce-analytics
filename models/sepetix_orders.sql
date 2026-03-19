ad106d19a38709e2ea0267c16990c8ee9ec8f391
zeliha tatar <zeyebebek@gmail.com>
2026-03-19 01:40:27 +0000

    Add sepetix orders model

--- models/sepetix_orders.sql
+++ models/sepetix_orders.sql
@@ -1,13 +1,13 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-SELECT
-  order_id,
-  customer_id,
-  product_name,
-  category,
-  quantity,
-  price,
-  order_date,
-  city,
-  status
-FROM `project-505e76f5-40a4-4295-b57.sepetix.orders`
+select
+    order_id,
+    customer_id,
+    product_name,
+    category,
+    quantity,
+    price,
+    order_date,
+    city,
+    status
+from `project-505e76f5-40a4-4295-b57.sepetix.orders`
